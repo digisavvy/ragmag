b98679023865247806937bfc3ca14dd98a14738d
alex vasquez <alex@digisavvy.com>
2014-11-29 08:50:14 -0800

    add author archive link

--- wp-content/themes/ragmag/page-templates/partials/content-home.php
+++ wp-content/themes/ragmag/page-templates/partials/content-home.php
@@ -4,9 +4,12 @@
 	<div class='articles'>
 		<a href="<?php echo get_permalink(); ?>" title="Read About <?php echo get_the_title( ); ?>"><?php echo get_the_title(); ?></a>
 		<br>
-		by <span class="the-author"><?php $author = get_the_author();
-				echo $author; ?></span>
-
+		by <span class="the-author">
+					<?php
+						$author = get_the_author();
+						echo '<a href="'.get_author_posts_url( get_the_author_meta( 'ID' ) ).'" title="An article by '.$author.'">'.$author.'</a>';
+					?>
+				</span>
 	</div>
 
 </article >
