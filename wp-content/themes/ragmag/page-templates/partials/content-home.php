e3287b15bdd9ea26c9a6006e78944849e25b99da
alex vasquez <alex@digisavvy.com>
2014-11-29 08:09:44 -0800

    adding post meta

--- wp-content/themes/ragmag/page-templates/partials/content-home.php
+++ wp-content/themes/ragmag/page-templates/partials/content-home.php
@@ -1,6 +1,12 @@
 
-<article class="post-id-<?php echo get_the_ID(); ?>">
+<article class="home post-id-<?php echo get_the_ID(); ?>">
 
-	<div class='articles'><?php echo get_the_title(); ?></div>
+	<div class='articles'>
+		<a href="<?php echo get_permalink(); ?>" title="Read About <?php echo get_the_title( ); ?>"><?php echo get_the_title(); ?></a>
+		<br>
+		by <span class="the-author"><?php $author = get_the_author();
+				echo $author; ?></span>
+
+	</div>
 
 </article >
