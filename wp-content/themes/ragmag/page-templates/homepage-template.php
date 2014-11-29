cd383d6a2d763c146b86c702664762103e84d8d5
alex vasquez <alex@digisavvy.com>
2014-11-29 00:46:59 -0800

    added home tmeplate partial

---
+++ wp-content/themes/ragmag/page-templates/homepage-template.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Template Name: Home Page Template
+ *
+ * This template display content at full with, with no sidebars.
+ * Please note that this is the WordPress construct of pages and that other 'pages' on your WordPress site will use a different template.
+ *
+ * @package digistarter
+ */
+
+get_header();
+
+	$args = array(
+		'post_status' 	       => 'publish',
+		'post_type'              => 'post',
+		'order'                  => 'ASC',
+		'orderby'                => 'menu_order',
+		'posts_per_page'				=> 6,
+	);
+
+	$blog_posts = new WP_Query($args);
+
+?>
+
+
+<div id="primary" class="content-area" role="main">
+
+<?php if($blog_posts->have_posts()) : ?>
+
+	 <?php while($blog_posts->have_posts()) : $blog_posts->the_post() ?>
+
+	 	<?php  get_template_part( 'page-templates/partials/content', 'home' ); ?>
+
+	 <?php endwhile ?>
+
+<?php endif ?>
+
+</div><!-- #primary -->
+
+
+<div id="secondary">
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+</div>
+<div id="tertiary">
+	<?php dynamic_sidebar( 'sidebar-2' ); ?>
+</div>
+
+<?php
+
+get_footer();
